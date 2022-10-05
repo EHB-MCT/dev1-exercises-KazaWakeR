e1b1ae6a534536e4a10e31314346d6cde8307c9c
yudjii <114521187+yudjii@users.noreply.github.com>
2022-10-05 12:30:18 +0200

    exercise progress

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,45 @@
 "use strict";
+
+drawName();
+
+function drawName() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   //Y
+
+   context.lineWidth = 5;
+   context.strokeStyle = 'red';
+   context.beginPath();
+   context.moveTo(300,50);
+   context.lineTo(400, 250);
+   context.lineTo(500,50);
+   context.stroke();
+
+   context.lineWidth = 5;
+   context.strokeStyle = 'red';
+   context.beginPath();
+   context.moveTo(300,450);
+   context.lineTo(450,150);
+   context.stroke();
+
+   //U
+
+   context.lineWidth = 5;
+   context.strokeStyle = 'red';
+   context.beginPath();
+   context.moveTo(450,450);
+   context.lineTo(450,150);
+   context.lineTo
+   context.stroke();
+
+
+}
+   
+
+   
+
+
