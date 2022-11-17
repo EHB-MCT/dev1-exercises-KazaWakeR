66582bcf9cd6eb072cc8d5d3d25cebb633bbc3a5
yudjii <114521187+yudjii@users.noreply.github.com>
2022-11-17 19:15:56 +0100

    opdracht

--- exercises/6-1-moving/Excercise 6-1: Crosshairs.js
+++ exercises/6-1-moving/Excercise 6-1: Crosshairs.js
@@ -5,3 +5,11 @@ import * as Utils from "../../scripts/utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
+
+window.onmousemove = drawCrosshair
+
+function drawCrosshair(event) {
+    console.log(event.pageX, event.pageY);
+    Utils.drawLine(event.pageX, 0, event.pageY, height);
+    Utils.drawLine(0, event.pageX, event.pageY, height);
+}
