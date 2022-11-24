3923f6a38968494e0d53fca5cc2c1f7c4064e86a
yudjii <114521187+yudjii@users.noreply.github.com>
2022-11-24 20:13:46 +0100

    maze done

--- exercises/5-1 exercise/5-2 maze.js
+++ exercises/5-1 exercise/5-2 maze.js
@@ -8,17 +8,27 @@ let height = context.canvas.height;
 
 let spacing = 10;
 
+
 drawMaze();
 
 function drawMaze() {
     context.lineWidth = 5;
-    context.lineCap = 'round';
+    context.lineCap = 'square';
     console.log('Checking up');
+
+    context.fillStyle = 'blue';
+
+    context.fillRect(0, 0, width, height);
+
+
+
 
     let horizontalAmaount = width / spacing;
     let verticalAmaount = height / spacing;
     for (let j = 0; j < verticalAmaount; j++) {
         for (let i = 0; i < horizontalAmaount; i++) {
+            let randomColor = Math.random() * 360;
+            context.strokeStyle = Utils.hsl(randomColor, 100, 50);
             let random = Math.floor(Math.random() * 2);
             if (random == 1) {
                 Utils.drawLine(i * spacing, j * spacing, spacing + i * spacing, spacing + j * spacing);
